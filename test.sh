7707bbbee30038a4e4e1e297046d7994315a9e6d
weide-zhou <weide-zhou@outlook.com>
2020-08-11 15:13:33 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,1 @@
-curl --request POST \
---url https://api.github.com/repos/weide-zhou/ticket30/issues \
---header 'authorization: Bearer $GH_TOKEN' \
---header 'content-type: application/json' \
---data '{
-          "title": "Automated issue for commit: $GH_SHA",
-          "body": "This issue was automatically created by the GitHub Action workflow. \n\n The commit hash was: $GH_SHA."
-          }'
+curl --request POST --url https://api.github.com/repos/weide-zhou/ticket30/issues --header 'authorization: Bearer $GH_TOKEN' --header 'content-type: application/json' --data '{ "title": "Automated issue for commit: $GH_SHA", "body": "This issue was automatically created by the GitHub Action workflow. \n\n The commit hash was: $GH_SHA." }'
