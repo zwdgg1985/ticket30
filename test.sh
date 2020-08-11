dbc693b6b175be4815321ba28f6b3047a60596e7
zwdgg1985 <59002671+zwdgg1985@users.noreply.github.com>
2020-08-11 16:17:43 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-curl -v -u admin:$GH_TOKEN -H "Accept: application/vnd.github.antiope-preview+json" -d '{"labels": ["TestMilestone"]}' https://api.github.com/repos/weide-zhou/ticket30/issues/14/labels
+curl -v -u admin:$GH_TOKEN -H "Accept: application/vnd.github.antiope-preview+json" -d '{"labels": ["TestMilestone"]}' https://api.github.com/repos/weide-zhou/ticket30/issues/15/labels
 echo $GH_TOKEN >> checktoken.txt
