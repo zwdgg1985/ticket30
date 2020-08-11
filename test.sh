8ad3d3f7e36926953c7a7cae20862ba64b555afc
weide-zhou <weide-zhou@outlook.com>
2020-08-11 15:16:35 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-curl --request POST --url https://api.github.com/repos/weide-zhou/ticket30/issues --header 'authorization: Bearer $GH_TOKEN' --header 'content-type: application/json' --data '{ "title": "Automated issue for commit: $GH_SHA", "body": "This issue was automatically created by the GitHub Action workflow. \n\n The commit hash was: $GH_SHA." }'
+curl -v -u admin:$GH_TOKEN -H "Accept: application/vnd.github.antiope-preview+json" -d '{"labels": ["Needs Milestone"]}' https://api.github.com/repos/weide-zhou/ticket30/issues/10/labels
